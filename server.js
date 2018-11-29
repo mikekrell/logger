7cbe370c5f556eacb1f1bd0a1d2af62c9775785b
Michael Krell <Michael.Krell@balfour.com>
2018-11-28 21:31:23 -0600

    teste2

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3000;
 var public = path.join(__dirname, 'public');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
+    const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.sendFile(path.join(public, 'index.html'));
 });
 
 app.use('/', express.static(public));
 
-app.listen(8080);
+app.listen(port);
