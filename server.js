2f9b84a3fc36839158bdc5252d34e1e442dbf581
Michael Krell <Michael.Krell@balfour.com>
2018-11-28 21:40:04 -0600

    testets

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var public = path.join(__dirname, 'public');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.sendFile(path.join(public, 'index.html'));
 });
 
